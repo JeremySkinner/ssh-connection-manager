f5c4458728b55260a47f8b5e6dedcf37b3f3a148
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-08-09 22:22:40 +0100

    Tidy up connection script

--- ssh-connect.sh
+++ ssh-connect.sh
@@ -1,8 +1,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-node $DIR/src/app.js list
+if [ "$1" == "" ]; then 
+  node $DIR/src/app.js list
+else 
+   node $DIR/src/app.js $@ 
+fi 
 
+# If the app created a connection file, load it and connect.
 if [ -f "/tmp/new_ssh_connection" ]; then
-   conn=$(</tmp/new_ssh_connection)
-   command ssh $conn
+  conn=$(</tmp/new_ssh_connection)
+  rm /tmp/new_ssh_connection
+  command ssh $conn
 fi
